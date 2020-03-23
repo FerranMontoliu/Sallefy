47172bb5ba333cf6281318c83992983a7e306c32
Ferran <ferran.montoliu@students.salle.url.edu>
2020-03-23 23:45:10 +0100

    Worked on Login Activity + Register Activity

--- app/src/main/java/com/example/sallefy/controller/activities/LoginActivity.java
+++ app/src/main/java/com/example/sallefy/controller/activities/LoginActivity.java
@@ -1,4 +1,68 @@
 package com.example.sallefy.controller.activities;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.sallefy.R;
+import com.example.sallefy.model.UserToken;
+import com.example.sallefy.restapi.callback.LoginCallback;
+import com.example.sallefy.restapi.manager.UserManager;
+import com.example.sallefy.utils.Session;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity implements LoginCallback {
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    private TextView tvLoginToRegister;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initViews();
+    }
+
+    private void initViews() {
+        etUsername = findViewById(R.id.login_username);
+        etPassword = findViewById(R.id.login_password);
+
+        tvLoginToRegister = findViewById(R.id.login_login_to_register);
+        tvLoginToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
+
+        btnLogin = findViewById(R.id.login_btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserManager.getInstance(getApplicationContext()).loginAttempt(etUsername.getText().toString(), etPassword.getText().toString(), LoginActivity.this);
+            }
+        });
+    }
+
+    @Override
+    public void onLoginSuccess(UserToken userToken) {
+        Session.getInstance(getApplicationContext()).setUserToken(userToken);
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
+
+    @Override
+    public void onLoginFailure(Throwable throwable) {
+        Toast.makeText(getApplicationContext(), R.string.login_failed, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onFailure(Throwable throwable) {
+        Toast.makeText(getApplicationContext(), R.string.exploded, Toast.LENGTH_LONG).show();
+    }
 }
--- app/src/main/java/com/example/sallefy/controller/activities/RegisterActivity.java
+++ app/src/main/java/com/example/sallefy/controller/activities/RegisterActivity.java
@@ -1,4 +1,65 @@
 package com.example.sallefy.controller.activities;
 
-public class RegisterActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.sallefy.R;
+import com.example.sallefy.model.UserRegister;
+import com.example.sallefy.restapi.callback.RegisterCallback;
+import com.example.sallefy.restapi.manager.UserManager;
+import com.example.sallefy.utils.Session;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity implements RegisterCallback {
+    private EditText etUsername;
+    private EditText etMail;
+    private EditText etPassword;
+    private Button btnRegister;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        initViews();
+    }
+
+    private void initViews() {
+        etUsername = findViewById(R.id.register_username);
+        etMail = findViewById(R.id.register_mail);
+        etPassword = findViewById(R.id.register_password);
+
+        btnRegister = findViewById(R.id.register_btn_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String login = etUsername.getText().toString();
+                String email = etMail.getText().toString();
+                String password = etPassword.getText().toString();
+
+                Session.getInstance(getApplicationContext()).setUserRegister(new UserRegister(email, login, password));
+                UserManager.getInstance(getApplicationContext()).registerAttempt(email, login, password, RegisterActivity.this);
+            }
+        });
+    }
+
+    @Override
+    public void onRegisterSuccess() {
+        Toast.makeText(getApplicationContext(), R.string.register_success, Toast.LENGTH_LONG).show();
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+    }
+
+    @Override
+    public void onRegisterFailure(Throwable throwable) {
+        Toast.makeText(getApplicationContext(), R.string.register_failed, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onFailure(Throwable throwable) {
+        Toast.makeText(getApplicationContext(), R.string.exploded, Toast.LENGTH_LONG).show();
+    }
 }
