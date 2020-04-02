11d03ce448351e7863ad664ca66629d5de6c3e0d
Ferran <ferran.montoliu@students.salle.url.edu>
2020-04-02 22:00:18 +0200

    #10 [WIP] Worked on displaying the user's basic info

---
+++ app/src/main/java/com/example/sallefy/controller/adapters/OwnUserAdapter.java
@@ -0,0 +1,64 @@
+package com.example.sallefy.controller.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.sallefy.R;
+import com.example.sallefy.model.User;
+
+
+public class OwnUserAdapter extends RecyclerView.Adapter<OwnUserAdapter.ViewHolder> {
+
+    public static final String TAG = OwnUserAdapter.class.getName();
+
+    private User mUsers;
+    private Context mContext;
+
+    public OwnUserAdapter(User user, Context context) {
+        mUsers = user;
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.tvUsername.setText(mUsers.getLogin());
+        if (mUsers.getImageUrl() != null) {
+            Glide.with(mContext)
+                    .asBitmap()
+                    .load(mUsers.getImageUrl())
+                    .into(holder.ivPhoto);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return (mUsers != null ? 1 : 0);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvUsername;
+        ImageView ivPhoto;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvUsername = itemView.findViewById(R.id.item_user_name);
+            ivPhoto = itemView.findViewById(R.id.item_user_photo);
+        }
+    }
+}
--- app/src/main/java/com/example/sallefy/controller/fragments/YourLibraryFragment.java
+++ app/src/main/java/com/example/sallefy/controller/fragments/YourLibraryFragment.java
@@ -14,18 +14,23 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.sallefy.R;
 import com.example.sallefy.controller.adapters.OwnPlaylistListAdapter;
+import com.example.sallefy.controller.adapters.OwnUserAdapter;
 import com.example.sallefy.controller.callbacks.PlaylistAdapterCallback;
 import com.example.sallefy.controller.restapi.callback.PlaylistCallback;
+import com.example.sallefy.controller.restapi.callback.UserCallback;
 import com.example.sallefy.controller.restapi.manager.PlaylistManager;
+import com.example.sallefy.controller.restapi.manager.UserManager;
 import com.example.sallefy.model.Playlist;
+import com.example.sallefy.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class YourLibraryFragment extends Fragment implements PlaylistCallback, PlaylistAdapterCallback {
+public class YourLibraryFragment extends Fragment implements PlaylistCallback, PlaylistAdapterCallback, UserCallback {
     public static final String TAG = YourLibraryFragment.class.getName();
 
     private RecyclerView mRecyclerView;
+    private RecyclerView userRV;
 
     public static YourLibraryFragment getInstance() {
         return new YourLibraryFragment();
@@ -42,8 +47,10 @@ public class YourLibraryFragment extends Fragment implements PlaylistCallback, P
         View v = inflater.inflate(R.layout.fragment_your_library, container, false);
 
         mRecyclerView = v.findViewById(R.id.playlists_rv);
+        userRV = v.findViewById(R.id.user_rv);
 
         PlaylistManager.getInstance(getContext()).getOwnPlaylists(YourLibraryFragment.this);
+        UserManager.getInstance(getContext()).getUserData("ferran.montoliu", YourLibraryFragment.this);
         return v;
     }
 
@@ -113,5 +120,13 @@ public class YourLibraryFragment extends Fragment implements PlaylistCallback, P
     @Override
     public void onPlaylistClick(Playlist playlist) {
         // TODO
+    }
+
+    @Override
+    public void onUserInfoReceived(User userData) {
+        LinearLayoutManager manager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
+        OwnUserAdapter adapter = new OwnUserAdapter(userData, getContext());
+        userRV.setLayoutManager(manager);
+        userRV.setAdapter(adapter);
     }
 }
