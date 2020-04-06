90a178346e9f1428c4c43141eb639eddcca64f6a
Guillem Garrofé Montoliu <guillem.garrofe@salle.url.edu>
2020-04-06 16:29:36 +0200

    #8 | Playing song view created

--- app/src/main/java/com/example/sallefy/controller/activities/PlayingSongActivity.java
+++ app/src/main/java/com/example/sallefy/controller/activities/PlayingSongActivity.java
@@ -1,4 +1,29 @@
 package com.example.sallefy.controller.activities;
 
-public class PlayingSongActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.sallefy.R;
+import com.example.sallefy.controller.restapi.manager.UserManager;
+import com.example.sallefy.model.UserRegister;
+import com.example.sallefy.utils.Session;
+
+public class PlayingSongActivity extends AppCompatActivity {
+    private ImageButton btnBack;
+    private ImageButton btnAdd;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playing_song);
+        initViews();
+    }
+
+    private void initViews() {
+        btnBack = findViewById(R.id.back_song);
+        btnAdd = findViewById(R.id.add_song);
+    }
 }
--- app/src/main/java/com/example/sallefy/controller/fragments/HomeFragment.java
+++ app/src/main/java/com/example/sallefy/controller/fragments/HomeFragment.java
@@ -1,15 +1,18 @@
 package com.example.sallefy.controller.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.sallefy.R;
+import com.example.sallefy.controller.activities.PlayingSongActivity;
 
 public class HomeFragment extends Fragment {
     public static final String TAG = HomeFragment.class.getName();
@@ -26,7 +29,17 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        Button btnTest = v.findViewById(R.id.test_song);
+
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), PlayingSongActivity.class));
+            }
+        });
+
+        return v;
     }
 
     @Override
