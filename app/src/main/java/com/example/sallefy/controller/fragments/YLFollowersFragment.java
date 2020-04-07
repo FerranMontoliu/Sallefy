0e8e8134789630de874b014273796191977ef1cb
Ferran <ferran.montoliu@students.salle.url.edu>
2020-04-07 21:05:10 +0200

    #12 Implemented the "Followers" fragment

--- app/src/main/java/com/example/sallefy/controller/fragments/YLFollowersFragment.java
+++ app/src/main/java/com/example/sallefy/controller/fragments/YLFollowersFragment.java
@@ -1,12 +1,74 @@
 package com.example.sallefy.controller.fragments;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class YLFollowersFragment extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.sallefy.R;
+import com.example.sallefy.controller.adapters.UserListAdapter;
+import com.example.sallefy.controller.callbacks.UserAdapterCallback;
+import com.example.sallefy.controller.restapi.callback.UserCallback;
+import com.example.sallefy.controller.restapi.manager.UserManager;
+import com.example.sallefy.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class YLFollowersFragment extends Fragment implements UserCallback, UserAdapterCallback {
     public static final String TAG = YLFollowersFragment.class.getName();
+
+    private RecyclerView mRecyclerView;
+
 
     public static YLFollowersFragment getInstance() {
         return new YLFollowersFragment();
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_yl_followers, container, false);
+
+        mRecyclerView = v.findViewById(R.id.users_rv);
+
+        UserManager.getInstance(getContext()).getFollowers(YLFollowersFragment.this);
+
+        return v;
+    }
+
+    @Override
+    public void onUserClick(User user) {
+        // TODO: OPEN USER FRAGMENT
+    }
+
+    @Override
+    public void onUserInfoReceived(User userData) {
+        // UNUSED
+    }
+
+    @Override
+    public void onUsersReceived(List<User> users) {
+        LinearLayoutManager manager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
+        UserListAdapter adapter = new UserListAdapter((ArrayList<User>) users, getContext(), YLFollowersFragment.this, R.layout.item_user_list);
+        mRecyclerView.setLayoutManager(manager);
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onFailure(Throwable throwable) {
+        Toast.makeText(getContext(), R.string.exploded, Toast.LENGTH_LONG).show();
+    }
 }
--- app/src/main/java/com/example/sallefy/controller/restapi/callback/UserCallback.java
+++ app/src/main/java/com/example/sallefy/controller/restapi/callback/UserCallback.java
@@ -2,6 +2,9 @@ package com.example.sallefy.controller.restapi.callback;
 
 import com.example.sallefy.model.User;
 
+import java.util.List;
+
 public interface UserCallback extends FailureCallback {
     void onUserInfoReceived(User userData);
+    void onUsersReceived(List<User> users);
 }
