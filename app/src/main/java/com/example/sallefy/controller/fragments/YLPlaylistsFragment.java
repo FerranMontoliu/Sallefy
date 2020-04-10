fe269c10ee22c52f1a13e96f0e4c64158e332ed9
Guillem Garrofé Montoliu <guillem.garrofe@salle.url.edu>
2020-04-10 20:54:13 +0200

    #12 | Your Library view fixed.

--- app/src/main/java/com/example/sallefy/controller/fragments/YLPlaylistsFragment.java
+++ app/src/main/java/com/example/sallefy/controller/fragments/YLPlaylistsFragment.java
@@ -56,6 +56,10 @@ public class YLPlaylistsFragment extends Fragment implements PlaylistCallback, P
         });
 
         mRecyclerView = v.findViewById(R.id.playlists_rv);
+        LinearLayoutManager manager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
+        OwnPlaylistListAdapter adapter = new OwnPlaylistListAdapter((ArrayList<Playlist>) null, getContext(), YLPlaylistsFragment.this, R.layout.item_own_playlist);
+        mRecyclerView.setLayoutManager(manager);
+        mRecyclerView.setAdapter(adapter);
 
         PlaylistManager.getInstance(getContext()).getOwnPlaylists(YLPlaylistsFragment.this);
 
@@ -129,9 +133,7 @@ public class YLPlaylistsFragment extends Fragment implements PlaylistCallback, P
 
     @Override
     public void onPlaylistsReceived(List<Playlist> playlists) {
-        LinearLayoutManager manager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
         OwnPlaylistListAdapter adapter = new OwnPlaylistListAdapter((ArrayList<Playlist>) playlists, getContext(), YLPlaylistsFragment.this, R.layout.item_own_playlist);
-        mRecyclerView.setLayoutManager(manager);
         mRecyclerView.setAdapter(adapter);
     }
 
