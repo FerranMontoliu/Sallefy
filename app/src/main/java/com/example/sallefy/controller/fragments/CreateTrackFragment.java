5ba6c7036b0eddb102ab83b4fc50dee47568f773
Ferran <ferran.montoliu@students.salle.url.edu>
2020-04-17 03:55:28 +0200

    #43 Now you can add an image with the gallery intent

--- app/src/main/java/com/example/sallefy/controller/fragments/CreateTrackFragment.java
+++ app/src/main/java/com/example/sallefy/controller/fragments/CreateTrackFragment.java
@@ -1,10 +1,16 @@
 package com.example.sallefy.controller.fragments;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,9 +18,18 @@ import androidx.fragment.app.Fragment;
 
 import com.example.sallefy.R;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Objects;
+
+import static android.app.Activity.RESULT_OK;
+
 public class CreateTrackFragment extends Fragment {
 
     public static final String TAG = CreateTrackFragment.class.getName();
+    private static final int PICK_IMAGE = 0;
+
+    private ImageView ivThumbnail;
 
     public static CreateTrackFragment getInstance() {
         return new CreateTrackFragment();
@@ -30,8 +45,39 @@ public class CreateTrackFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_create_track, container, false);
 
-        // TODO
+        v.findViewById(R.id.upload_thumbnail_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, PICK_IMAGE);
+            }
+        });
+
+        ivThumbnail = v.findViewById(R.id.thumbnail_iv);
+
 
         return v;
+    }
+
+    @Override
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                assert imageUri != null;
+                final InputStream imageStream = Objects.requireNonNull(getActivity()).getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                ivThumbnail.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(getContext(), R.string.exploded, Toast.LENGTH_LONG).show();
+            }
+
+        } else {
+            Toast.makeText(getContext(), R.string.pick_an_image, Toast.LENGTH_LONG).show();
+        }
     }
 }
