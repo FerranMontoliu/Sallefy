3e32761627b77e7e1ff09e94ddbc9f87227ae872
Ferran <ferran.montoliu@students.salle.url.edu>
2020-06-01 02:27:58 +0200

    #93 | Now you can share a track with its thumbnail, text with emojis and the url

--- app/src/main/java/com/example/sallefy/fragment/TrackOptionsFragment.java
+++ app/src/main/java/com/example/sallefy/fragment/TrackOptionsFragment.java
@@ -1,20 +1,40 @@
 package com.example.sallefy.fragment;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.bumptech.glide.Glide;
 import com.example.sallefy.R;
+import com.example.sallefy.activity.MainActivity;
 import com.example.sallefy.databinding.FragmentTrackOptionsBinding;
 import com.example.sallefy.factory.ViewModelFactory;
 import com.example.sallefy.viewmodel.TrackOptionsViewModel;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import javax.inject.Inject;
 
@@ -61,7 +81,7 @@ public class TrackOptionsFragment extends DaggerFragment {
         binding.ftoTrackNameTv.setText(trackOptionsViewModel.getTrack().getName());
 
         if (trackOptionsViewModel.getTrack().getThumbnail() != null) {
-            Glide.with(getContext())
+            Glide.with(requireContext())
                     .asBitmap()
                     .placeholder(R.drawable.ic_audiotrack_60dp)
                     .load(trackOptionsViewModel.getTrack().getThumbnail())
@@ -72,11 +92,58 @@ public class TrackOptionsFragment extends DaggerFragment {
             trackOptionsViewModel.downloadTrackToggle();
         });
 
+        binding.ftoShareRl.setOnClickListener(v -> {
+            checkForPermissions();
+        });
+
+    }
+
+    private void checkForPermissions() {
+        int permissionWrite = ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permissionRead = ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (permissionWrite != PackageManager.PERMISSION_GRANTED || permissionRead != PackageManager.PERMISSION_GRANTED)
+            askForPermission();
+        else
+            shareTrackLink();
+    }
+
+    private void askForPermission() {
+        ActivityCompat.requestPermissions(requireActivity(),
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+    }
+
+    private void shareTrackLink() {
+        Bitmap bitmap = getBitmapFromView(binding.ftoThumbnailIv);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, bos);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("image/*");
+        String path = MediaStore.Images.Media.insertImage(requireContext().getContentResolver(), bitmap, "shared_image", null);
+        Uri imageUri = Uri.parse(path);
+        intent.putExtra(Intent.EXTRA_TEXT, getResources().getString(R.string.share_song_text) + trackOptionsViewModel.getTrack().getId());
+        intent.putExtra(Intent.EXTRA_STREAM, imageUri);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivity(Intent.createChooser(intent, getResources().getString(R.string.share_track_via)));
+    }
+
+
+    private Bitmap getBitmapFromView(View view) {
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnedBitmap);
+        Drawable bgDrawable = view.getBackground();
+        if (bgDrawable != null)
+            bgDrawable.draw(canvas);
+        else
+            canvas.drawColor(Color.WHITE);
+
+        view.draw(canvas);
+        return returnedBitmap;
     }
 
     private void subscribeObservers() {
-        trackOptionsViewModel.isDownloaded().observe(getViewLifecycleOwner(), isDownloaded ->{
-            if (isDownloaded){
+        trackOptionsViewModel.isDownloaded().observe(getViewLifecycleOwner(), isDownloaded -> {
+            if (isDownloaded) {
                 binding.imageView4.setImageDrawable(getResources().getDrawable(R.drawable.ic_downloaded, getActivity().getTheme()));
                 binding.textView5.setText(R.string.downloaded);
                 binding.textView5.setTextColor(getResources().getColor(R.color.green, getActivity().getTheme()));
@@ -86,5 +153,23 @@ public class TrackOptionsFragment extends DaggerFragment {
                 binding.textView5.setTextColor(getResources().getColor(R.color.light, getActivity().getTheme()));
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                    shareTrackLink();
+                } else {
+                    // Permission denied
+                    Toast.makeText(requireContext(), R.string.error_external_permission, Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
     }
 }
